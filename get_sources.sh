ec866dfc08607dea83033f369fdbafc64a388ffd
Tigro <ashejn@gmail.com>
2017-03-09 17:57:28 +0300

    update to 25.19

--- get_sources.sh
+++ get_sources.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
-curl -sO http://pkgs.fedoraproject.org/repo/pkgs/lorax/lorax-25.17.tar.gz/e951c499404a43e537e0db72f39467e0/lorax-25.17.tar.gz
+curl -sO http://pkgs.fedoraproject.org/repo/pkgs/lorax/lorax-25.19.tar.gz/sha512/b68db942268badd6e4795d1f0116b2d1087818a3b61ea2710d808f03ad2f6abb3ab36953ab8f699dc3a2c97bf83734fd69c1fa0595b7f1f4c2c71eb664eabe58/lorax-25.19.tar.gz
 
