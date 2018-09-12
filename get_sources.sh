40d20d4a8ac1423fd65e770053412e7dd381082a
Tigro <ashejn@gmail.com>
2018-09-12 23:08:38 +0300

    update to 29.12

--- get_sources.sh
+++ get_sources.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
-curl -sO https://src.fedoraproject.org/repo/pkgs/lorax/lorax-28.15.tar.gz/sha512/36b4c03c0713affdc40f9733faad5dbefcc31040a5d0f386cae639d85198d2a37a0e115a0791b9f5cc97c30ca27e6dfeb37e4d016087e6509239740df3be65d8/lorax-28.15.tar.gz
+curl -sO https://src.fedoraproject.org/repo/pkgs/lorax/lorax-29.12.tar.gz/sha512/f81e9556400cb1348b969d7407c51694649efddf9376f59906fd3310d24def8be7a41eb72b7b9198b7739547e8bd2556f5bd53a8dd9c70d7e2da3ba3904213f4/lorax-29.12.tar.gz
 
